65c5b3876d65d55a7b3f59903c3a24314d9a9356
Simeodey <adamand12@abv.bg>
2022-12-09 19:12:37 +0200

    das

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+  const productDiv = document.querySelector('.product');
+  const productPrice = document.querySelectorAll('p')[1].innerHTML;
+  const span = document.createElement('span');
+  span.className = "data-price";
+  span.innerText = productPrice;
+  productDiv.setAttribute('data-price', productPrice);
+  productDiv.appendChild(span);
 });
